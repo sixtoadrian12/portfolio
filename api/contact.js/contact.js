7c7538b0e5ed58d28915224973432be762ed47cf
sixtoadrian12 <sixto.adrian12@gmail.com>
2025-12-04 00:55:11 +0800

    Update contact.js

--- api/contact.js/contact.js
+++ api/contact.js/contact.js
@@ -1,6 +1,9 @@
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { name, email, message } = req.body;
+    if (!name || !email || !message || message.replace(/\s/g, '').length < 5) {
+      return res.status(400).json({ success: false, error: "Invalid message" });
+    }
     console.log("Message received:", { name, email, message });
     return res.status(200).json({ success: true, message: "Message received!" });
   } else {
